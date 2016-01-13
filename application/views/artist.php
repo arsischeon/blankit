d7192286282fe57a427cb2427bd783787dbb21c7
ArsisCheon <arsischeon@gmail.com>
2016-01-13 16:39:43 +0900

    edit artist

--- application/views/artist.php
+++ application/views/artist.php
@@ -10,18 +10,18 @@
 	<div class="artist-row">
 		<div class="artist-preProfile col-md-3">
 			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail">
-			<p>아티스트D</p>
-			<p>그래픽 디자인</p>
-			<p>더보기</p>
+			<p>Artist D</p>
+			<p>Graphic Design</p>
+			<button type="button" class="btn btn-default">Default</button>
 		</div>
 		<div class="artist-preImages col-md-9">
-			<div class="artist-preImage col-md-4">
+			<div class="artist-preImage">
 				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 			</div>
-			<div class="artist-preImag col-md-4">
+			<div class="artist-preImage">
 				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 			</div>
-			<div class="artist-preImage col-md-4">
+			<div class="artist-preImage">
 				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 			</div>
 		</div>
