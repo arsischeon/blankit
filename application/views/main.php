6e9cd5dbdbdaed01cfa87a1cb019074b9097b824
Woohyun Kim <r45r54r45@naver.com>
2016-01-13 14:58:47 +0900

    navbar 작업 중

--- application/views/main.php
+++ application/views/main.php
@@ -1,9 +1,13 @@
 <nav class="navbar navbar-default">
-  <div class="container-fluid">
+  <div class="container">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">
-        <img alt="blankit" src="/source/image/logo.png">
+        <img id="logo" alt="blankit" src="source/image/logo.png">
       </a>
+
     </div>
+    <p class="navbar-text">about us</p>
+    <p class="navbar-text">artist</p>
+    <p class="navbar-text">store</p>
   </div>
 </nav>
