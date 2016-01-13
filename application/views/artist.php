cdaf6195cc866e211f3cb38de1aec7a834a8d2e7
ArsisCheon <arsischeon@gmail.com>
2016-01-13 16:15:08 +0900

    test artist

--- application/views/artist.php
+++ application/views/artist.php
@@ -2,22 +2,23 @@
 .thumbnail{
 	width: 150px;
 	height: 150px;
+	padding: 0;
 }
 </style>
 
 <div class="artist-content">
 	<div class="artist-row">
 		<div class="artist-preProfile col-md-3">
-			.col-sm-3
+			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail">
 		</div>
 		<div class="artist-preImages col-md-3">
-			.col-sm-3
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 		</div>
 		<div class="artist-preImage col-md-3">
-			.col-sm-3
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 		</div>
 		<div class="artist-preImage col-md-3">
-			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail"></a>
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
 		</div>
 	</div>
 </div>
