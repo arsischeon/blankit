7c7b904448b6ba5b06701d2de4960b3f083545c4
ArsisCheon <arsischeon@gmail.com>
2016-01-13 17:21:53 +0900

    edit artist

--- application/views/artist.php
+++ application/views/artist.php
@@ -13,11 +13,9 @@
 	<div class="row">
 		<div class="preProfile col-md-3">
 			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail img-circle">
-			<p>Artist D</p>
+			<p>ì•„í‹°ìŠ¤íŠ¸ D</p>
 			<p>Graphic Design</p>
-			<p>±×·¡ÇÈµğÀÚÀÎ</p>
-			<hr />
-			<button type="button" class="btn btn-default">Default</button>
+			<button type="button" class="btn btn-default">ë” ë³´ê¸°</button>
 		</div>
 		<div class="preImages col-md-9">
 			<div class="preImage">
