d22bfef8a76ae83b26cc4cb84fb835359e42e044
Woohyun Kim <r45r54r45@naver.com>
2016-01-13 20:21:41 +0900

    ddd

--- application/views/main.php
+++ application/views/main.php
@@ -7,36 +7,48 @@
     overflow: hidden;
     -webkit-backface-visibility: hidden;
     background: grey;
+    z-index: 100;
   }
   .image-in-banner{
+    width:100%;
     transform: rotate(7deg);
-    width: 100%;
-    left:0px;
     -webkit-backface-visibility: hidden;
   }
 </style>
 <script>
-$(window).load(function(){
-  var height=$(window).height()*0.7;
+function slanted_banner_generator(){
+  var height=$(window).height()*0.8;
   var angle=7*Math.PI/180;
   var banner_height=height*Math.cos(angle);
   var banner_width=$(window).width()*Math.cos(angle)+height*Math.sin(angle);
-
-  // var banner_left=-1*banner_height*Math.sin(angle);
-  // var banner_top=-1*(banner_width-banner_height*Math.tan(angle))*Math.sin(angle);
-
   $(".banner").css("left","0px");
   $(".banner").css("top","0px");
   $(".banner").css("width",banner_width+"px");
   $(".banner").css("height",banner_height+"px");
+  $(".banner-wrap").css("height",banner_height-50+"px");
+
+}
+$(function(){
+  $(".banner").css("display","none");
+  slanted_banner_generator();
+  $(".banner").css("display","");
+  $(".banner").hover(function(){
+    $(".banner").css("background","red");
+  });
+});
+$(window).resize(function(){
+  slanted_banner_generator();
 });
 </script>
+<div class="banner-wrap hidden-xs ">
 <div class="banner">
-  <!-- <img class="image-in-banner " src="/source/image/banner1.jpg"> -->
+  <!-- <img class="image-in-banner" src="/source/image/banner1.jpg"> -->
 </div>
-<div class="row">
-<div class="well">
-  <span>few</span>
-
+<div class="banner-roll">
+</div>
+</div>
+<div class="row" style="background:red;">
+  <div class="col-md-12">
+  <span>ve</span>
 </div>
 </div>
