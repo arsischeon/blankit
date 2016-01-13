1632e8e379e47d0fe89936c01de7bf24fc1913a9
ArsisCheon <arsischeon@gmail.com>
2016-01-13 16:03:13 +0900

    test artist

--- application/views/artist.php
+++ application/views/artist.php
@@ -1,16 +1,16 @@
 <div class="artist-content">
 	<div class="artist-row">
 		<div class="artist-pre-profile col-md-3">
-			<a target="_blank" href="#" class="artist-pre-profile-"
+			.col-sm-3
 		</div>
 		<div class="artist-pre-images col-md-3">
-			<img class="artist-pre-image" src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg">
+			.col-sm-3
 		</div>
 		<div class="artist-pre-image col-md-3">
-			<img class="artist-pre-image" src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg">
+			.col-sm-3
 		</div>
 		<div class="artist-pre-image col-md-3">
-			<img class="artist-pre-image" src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg">
+			.col-sm-3
 		</div>
 	</div>
 </div>
