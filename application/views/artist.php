9f569bb2b3c5c50bc7097c690177ba10e6ce13c5
ArsisCheon <arsischeon@gmail.com>
2016-01-13 21:28:56 +0900

    preimages color change

--- application/views/artist.php
+++ application/views/artist.php
@@ -64,10 +64,10 @@
     position:absolute;
     width:100%; height:100%;
     top:0; left:0;
-    background:rgba(0,0,0,0.6);
+    background:rgba(150,175,181,0.6);
     opacity:0;
-    transition: all 0.5s;
-    -webkit-transition: all 0.5s;
+    transition: all 0.3s;
+    -webkit-transition: all 0.3s;
 }
 .preImages:hover:after {
 	opacity: 1;
