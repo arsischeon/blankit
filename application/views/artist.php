dc901c654e64783f8c18cad0967e120d69cbebdd
ArsisCheon <arsischeon@gmail.com>
2016-01-13 19:17:29 +0900

    block3

--- application/views/artist.php
+++ application/views/artist.php
@@ -67,6 +67,24 @@
 		<div class="preImages col-md-3 col-xs-4">
 			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
 		</div>
+		
+	</div>
+		<div class="row">
+		<div id="kk" class="preProfile col-md-3 col-xs-12">
+			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail img-circle">
+			<b class="artist-name">아티스트 D</b>
+			<p class="artist-type">그래픽 디자인</p>
+			<a href="#" class="myButton">더 보기</a>
+		</div>
+		<div class="preImages col-md-3 col-xs-4">
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
+		</div>
+		<div class="preImages col-md-3 col-xs-4">
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
+		</div>
+		<div class="preImages col-md-3 col-xs-4">
+			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
+	
 		</div>
 	</div>
 		<div class="row">
@@ -84,25 +102,7 @@
 		</div>
 		<div class="preImages col-md-3 col-xs-4">
 			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
-		</div>
-		</div>
-	</div>
-		<div class="row">
-		<div id="kk" class="preProfile col-md-3 col-xs-12">
-			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail img-circle">
-			<b class="artist-name">아티스트 D</b>
-			<p class="artist-type">그래픽 디자인</p>
-			<a href="#" class="myButton">더 보기</a>
-		</div>
-		<div class="preImages col-md-3 col-xs-4">
-			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
-		</div>
-		<div class="preImages col-md-3 col-xs-4">
-			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
-		</div>
-		<div class="preImages col-md-3 col-xs-4">
-			<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
-		</div>
+		
 		</div>
 	</div>
 </div>
