28e796d19d0a8d8d9180237a946e225150337cd7
Woohyun Kim <r45r54r45@naver.com>
2016-01-13 14:13:15 +0900

    main page navbar

---
+++ application/views/main.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="blankit" src="/source/image/logo.png">
+      </a>
+    </div>
+  </div>
+</nav>
