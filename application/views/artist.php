e5e2b51691284dd5a89ea2cf1da10bed7f65f993
ArsisCheon <arsischeon@gmail.com>
2016-01-13 21:27:08 +0900

    preimages fix

--- application/views/artist.php
+++ application/views/artist.php
@@ -60,7 +60,14 @@
 	height: auto;
 }
 .preImages:after {
-	background:rgba(0, 0, 0, 0.6);
+    content:'\A';
+    position:absolute;
+    width:100%; height:100%;
+    top:0; left:0;
+    background:rgba(0,0,0,0.6);
+    opacity:0;
+    transition: all 0.5s;
+    -webkit-transition: all 0.5s;
 }
 .preImages:hover:after {
 	opacity: 1;
