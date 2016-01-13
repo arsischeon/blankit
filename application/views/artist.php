fc385696bb6717050e9ea198352f8a5c72558fce
ArsisCheon <arsischeon@gmail.com>
2016-01-13 19:44:19 +0900

    bold

--- application/views/artist.php
+++ application/views/artist.php
@@ -16,6 +16,7 @@
 	text-align: center;
 	font-family:"Noto Sans", "Noto Sans CJK KR", sans-serif;
 	font-size:15px;
+	font-wieght: bold;
 }
 .artist-type{
 	text-align: center;
@@ -65,7 +66,7 @@
 	<div class="row">
 		<div id="artistID" class="artist col-md-3 col-xs-12">
 			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="img-circle">
-			<b class="artist-name">아티스트 D</b>
+			<p class="artist-name">아티스트 D</p>
 			<p class="artist-type">그래픽 디자인</p>
 			<a href="#" class="myButton">더 보기</a>
 		</div>
