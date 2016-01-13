6aac697faf8d718d8538dbb6ce0ec35e832592e9
ArsisCheon <arsischeon@gmail.com>
2016-01-13 16:12:26 +0900

    test artist image

--- application/views/artist.php
+++ application/views/artist.php
@@ -1,16 +1,23 @@
+<style>
+.thumbnail{
+	width: 150px;
+	height: 150px;
+}
+</style>
+
 <div class="artist-content">
 	<div class="artist-row">
-		<div class="artist-pre-profile col-md-3">
+		<div class="artist-preProfile col-md-3">
 			.col-sm-3
 		</div>
-		<div class="artist-pre-images col-md-3">
+		<div class="artist-preImages col-md-3">
 			.col-sm-3
 		</div>
-		<div class="artist-pre-image col-md-3">
+		<div class="artist-preImage col-md-3">
 			.col-sm-3
 		</div>
-		<div class="artist-pre-image col-md-3">
-			.col-sm-3
+		<div class="artist-preImage col-md-3">
+			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail"></a>
 		</div>
 	</div>
 </div>
