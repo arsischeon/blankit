d972372021f9fbadcdc0582a15abd9ba568779ef
Woohyun Kim <r45r54r45@naver.com>
2016-01-13 15:23:41 +0900

    navbar 및 header에 content-area 만듬.

--- application/views/header.php
+++ application/views/header.php
@@ -18,3 +18,17 @@
     <![endif]-->
   </head>
   <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <img id="logo" alt="blankit" src="source/image/logo.png">
+          </a>
+
+        </div>
+        <p class="navbar-text">about us</p>
+        <p class="navbar-text">artist</p>
+        <p class="navbar-text">store</p>
+      </div>
+    </nav>
+    <div class="content-area">
