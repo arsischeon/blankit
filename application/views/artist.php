e7c274f78219ef27f6b287cd4668683a2591abc4
Woohyun Kim <r45r54r45@naver.com>
2016-01-13 17:35:24 +0900

    col

--- application/views/artist.php
+++ application/views/artist.php
@@ -1,31 +1,36 @@
 <style>
 
-.preImage {
+/*.preImage {
     display: inline-block;
     margin: 0 40px 0 0;
-}
+}*/
 .img-circle {
 	border-radius:50%;
+}
+#kk:col-xs-12{
+	display:block;
+	text-align:center;
 }
 </style>
 
 <div class="container">
 	<div class="row">
-		<div class="preProfile col-md-3">
+		<div id="kk" class="preProfile col-md-3 col-xs-12">
 			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail img-circle">
 			<p>아티스트 D</p>
 			<p>Graphic Design</p>
 			<button type="button" class="btn btn-default">더 보기</button>
 		</div>
-		<div class="preImages col-md-9">
-			<div class="preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
+		<div class="preImages col-md-3 col-xs-4">
+
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
+</div>
+		<div class="preImages col-md-3 col-xs-4">
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
+
 			</div>
-			<div class="preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
-			</div>
-			<div class="preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
+					<div class="preImages col-md-3 col-xs-4">
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img img-responsive img-rounded">
 			</div>
 		</div>
 	</div>
