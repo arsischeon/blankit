40acac69434f13934a01d1bf081489a8a8147b49
ArsisCheon <arsischeon@gmail.com>
2016-01-13 16:43:14 +0900

    edit artist

--- application/views/artist.php
+++ application/views/artist.php
@@ -4,25 +4,29 @@
 	height: 150px;
 	padding: 0;
 }
+.preview {
+    display: inline-block;
+    margin: 0 40px 0 0;
+}
 </style>
 
 <div class="artist-content">
 	<div class="artist-row">
-		<div class="artist-preProfile col-md-3">
-			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail">
+		<div class="preProfile col-md-3">
+			<img src="https://mir-s3-cdn-cf.behance.net/user/138/ff443554023.55072219ce354.jpg" class="thumbnail img-circle">
 			<p>Artist D</p>
 			<p>Graphic Design</p>
 			<button type="button" class="btn btn-default">Default</button>
 		</div>
-		<div class="artist-preImages col-md-9">
-			<div class="artist-preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
+		<div class="preImages col-md-9">
+			<div class="preImage">
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
 			</div>
-			<div class="artist-preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
+			<div class="preImage">
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
 			</div>
-			<div class="artist-preImage">
-				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail">
+			<div class="preImage">
+				<img src="https://mir-s3-cdn-cf.behance.net/projects/202/fab06432280103.Y3JvcCw4NDAsNjU3LDEsMA.jpg" class="thumbnail img-rounded">
 			</div>
 		</div>
 	</div>
