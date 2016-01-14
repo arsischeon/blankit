1205190781dc344c879e5a9e2507eca02e98682a
ArsisCheon <arsischeon@gmail.com>
2016-01-14 14:32:59 +0900

    fix

--- application/views/artist.php
+++ application/views/artist.php
@@ -64,8 +64,8 @@
 
 </div>
 
-<div class="row" style="margin-bottom: 40px; position: absolute;">
-	<div class="row-md-12" style="background: #F5F6F7; padding-top:30px; padding-bottom:30px; height: 500px;"></div>
+<div class="row" style="margin-bottom: 40px; position:absolute; left: 0px; top: 0px; width: 100%">
+	<div class="col-md-12" style="background: #F5F6F7; height: 500px;"></div>
 </div>
 
 <div class="container" style="margin: 0 auto;">
