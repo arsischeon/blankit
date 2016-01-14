dd6926f73331aec628af43fa46a64b050ff80d17
ArsisCheon <arsischeon@gmail.com>
2016-01-14 14:02:39 +0900

    fix div

--- application/views/artist.php
+++ application/views/artist.php
@@ -121,7 +121,7 @@
 	<hr>
 
 </div>
-
+</div>
 <div class="row" style="margin-top: 20px;">
 	<div class="row-md-12" style="background: #F5F6F7; padding-top:30px; padding-bottom:30px;">this is banner</div>
 </div>
