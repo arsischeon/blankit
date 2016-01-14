3dde4b05d5f25ea06a550477abb4083b31a265fc
ArsisCheon <arsischeon@gmail.com>
2016-01-14 14:59:24 +0900

    ratio

--- application/views/artist.php
+++ application/views/artist.php
@@ -56,13 +56,13 @@
 }*/
 .thumbnail {
 	margin: 0;
-	padding-top: 56.25%;
+	width: 100%;
+	height: auto;
 	border-radius: 15px;
 }
 .preImages {
 	padding-left: 10px;
 	padding-right: 10px;
-	width: 100%;
 }
 </style>
 
