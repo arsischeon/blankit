43e2496514f22d4fcd4ef8191d7dadbd805b18f3
ArsisCheon <arsischeon@gmail.com>
2016-01-14 13:59:21 +0900

    fix

--- application/views/artist.php
+++ application/views/artist.php
@@ -60,10 +60,11 @@
 	height: auto;
 }
 </style>
-
+<body>
 <div class="row" style="margin-top: 20px;">
 	<div class="row-md-12" style="background: #F5F6F7; padding-top:30px; padding-bottom:30px;">this is banner</div>
 </div>
+</body>
 
 <div class="container" style="margin: 0 auto;">
 	<div class="row">
