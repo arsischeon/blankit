4cffd76562cceb47bd7b1de7b4424fed82da71ac
Woohyun Kim <r45r54r45@naver.com>
2016-01-14 15:49:29 +0900

    header finish

--- application/views/header.php
+++ application/views/header.php
@@ -12,6 +12,7 @@
     <title>Blank.It</title>
     <script src="https://code.jquery.com/jquery-2.2.0.min.js" crossorigin="anonymous" ></script>
     <script src="/source/js/common.js" ></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="/source/css/common.css">
@@ -23,21 +24,51 @@
     <![endif]-->
   </head>
   <body style="  display: none;">
+
     <nav class="navbar navbar-default">
-      <div class="container">
+      <div class="container-fluid">
+
         <div class="navbar-header">
+
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
           <a class="navbar-brand" href="#">
             <img id="logo" alt="blankit" src="source/image/logo.png">
           </a>
-
         </div>
-        <p class="navbar-text navbar-left ">about us</p>
-        <p class="navbar-text navbar-left">artist</p>
-        <p class="navbar-text navbar-left">store</p>
-        <p class="navbar-text navbar-right">submit</p>
-        <p class="navbar-text navbar-right">mypage</p>
-        <p class="navbar-text navbar-right">join</p>
-        <p class="navbar-text navbar-right">login</p>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">about us</span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">서비스 소개</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">고객센터</a></li>
+                </ul>
+            </li>
+            <li><a class="navbar-left" href="#">artist</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">store</span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">의류</a></li>
+                  <li><a href="#">에코백</a></li>
+                  <li><a href="#">파우치</a></li>
+                </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a class="navbar-right" href="#">login</a></li>
+            <li><a class="navbar-right" href="#">join</a></li>
+            <li><a class="navbar-right" href="#">mypage</a></li>
+            <li><a style="margin-right:40px;" class="navbar-right" href="#">submit</a></li>
+          </ul>
+    </div>
+
       </div>
     </nav>
     <div class="container">
