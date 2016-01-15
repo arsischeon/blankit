ff37857089f587a945969486b1bac7f7ae88edad
Woohyun Kim <r45r54r45@naver.com>
2016-01-15 15:19:21 +0900

    dd

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,8 +6,8 @@ class Main extends CI_Controller {
 	public function index()
 	{
 		$this->load->model('main');
-		$data['recent']=$this->main->recent();
-		$data['artist']=$this->main->artist();
+		// $data['recent']=$this->main->recent();
+		// $data['artist']=$this->main->artist();
 		$this->load->view('header');
 		$this->load->view('main',$data);
 		$this->load->view('footer');
