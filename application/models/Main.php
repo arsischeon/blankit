86e15816fafbc684e6bf099fe505173e2a80e6e2
ArsisCheon <arsischeon@gmail.com>
2016-01-15 15:22:11 +0900

    fix Model

--- application/models/Main.php
+++ application/models/Main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends CI_Models{
+class Main extends CI_Model{
 	function recent(){
 		return mysql_query("");
 	}
