88d0cd42ca518a931ba8a151a50b47a67d27d7dd
ArsisCheon <arsischeon@gmail.com>
2016-01-16 17:00:15 +0900

    test db

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main_model extends CI_Model{
 	public function mdpick(){
-	return mysqli_query("
+	return $this->db->query("
 				select store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile
 				from MDPICK m, STORE s, USER u
 				where m.store_id = s.store_id and s.user_id = u.user_id;
