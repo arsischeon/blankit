a028a21dd96c646fe726fd3280da729e34f1ed0a
ArsisCheon <arsischeon@gmail.com>
2016-01-16 15:17:43 +0900

    mdpick

--- application/models/Main.php
+++ application/models/Main.php
@@ -3,11 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Model{
 	function mdpick(){
-		return mysql_query("
-				
+	return mysql_query("
+				select store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile
+				from MDPICK m, STORE s, USER u
+				where m.store_id = s.store_id and s.user_id = u.user_id;
 				");
 	}
-	/*select store_image, store_day, store_goal, store_goal_now, user_id
-				from MDPICK join STORE
-				on MDPICK.store_id = STORE.store_id;*/
 }
