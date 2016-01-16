e0a57a6b5bd10fecbc31a03e878d8a7b7b815fee
ArsisCheon <arsischeon@gmail.com>
2016-01-16 16:47:16 +0900

    test

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -2,8 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main_model extends CI_Model{
-	function recent(){
-		return mysql_query("");
+	function mdpick(){
+	return mysql_query("
+				select store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile
+				from MDPICK m, STORE s, USER u
+				where m.store_id = s.store_id and s.user_id = u.user_id;
+				");
 	}
-
 }
