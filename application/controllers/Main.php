4610a369a7c1d301f551d2a03c8f1a9cbc6bbf4c
ArsisCheon <arsischeon@gmail.com>
2016-01-16 16:44:19 +0900

    test

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,7 +6,7 @@ class Main extends CI_Controller {
 	public function index()
 	{
 		$this->load->model('main_model');
-		$data['mdpick']=$this->main->mdpick();
+		//$data['mdpick']=$this->main->mdpick();
 		// $data['mdartist']=$this->main->mdartist();
 		$this->load->view('header');
 		$this->load->view('main');
