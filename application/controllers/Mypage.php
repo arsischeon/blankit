1e772bc62e146b3ddd74b182c0aa96233b47f238
Woohyun Kim <r45r54r45@naver.com>
2016-01-19 15:52:53 +0900

    ㄴ

---
+++ application/controllers/Mypage.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mypage extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('header');
+		$this->load->view('mypage_order');
+		$this->load->view('footer');
+	}
+	public function complete()
+	{
+		$this->load->view('header');
+		$this->load->view('order_complete');
+		$this->load->view('footer');
+	}
+}
