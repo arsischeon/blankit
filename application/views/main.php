9a10b157155085139f4cd2dfec4177bb42830854
Woohyun Kim <r45r54r45@naver.com>
2016-01-19 18:09:36 +0900

    dd

--- application/views/main.php
+++ application/views/main.php
@@ -41,10 +41,10 @@
 
   .view_more{
     float: right;
-    border-bottom: 2px solid black;
+    border-bottom: 2px solid #687C87;
     text-align: left;
-    width:100px; //width 를 1px씩 증가시키면서 margin-right도 같이 감소하도록
-    margin-right: -50px;
+    margin-right: 5px;
+    color:#687C87;
   }
   .col-md-five{
     width:19.6%;
@@ -236,7 +236,8 @@ $(window).resize(function(){
 <div class="row" style="margin-top:20px;">
   <div class="col-md-12">
   <p class="row_title">STORE</p>
-  <p class="row_desc">이 카테고리를 설명할 수 있는 글이에요</p>
+  <p class="row_desc">오로지 블랭킷에서만, 오로지 이 기간 동안에만 만나볼 수 있는<br>
+독립 아티스트들의 아트워크를 확인해 보세요.</p>
 </div>
 </div>
 <div class="row">
@@ -358,11 +359,11 @@ $(window).resize(function(){
 
 <div class="container-fluid">
 <div class="row" style="margin-top: 20px;">
-  <div class="row-md-12" style="background: #F5F6F7; padding-top:40px; padding-bottom:50px;">
-    <p class="gal" style="font-size:21px; font-weight:bold; text-align:center; margin-top:15px;">당신도 함께해주세요!</p>
-    <p class="gal" style="font-size:13px; font-weight:regular; text-align:center;">포트폴리오에 묵혀둔 작품이 있나요? 제작비용 0원에 당신만의 제품을 선보이세요.<br>
+  <div class="row-md-12" style="background: #F5F6F7; padding-top:30px; padding-bottom:40px;">
+    <p class="gal" style="font-size:21px; font-weight:bold; text-align:center; margin-top:15px; margin-bottom:15px;">당신도 함께해주세요!</p>
+    <p class="gal" style="font-size:13px; font-weight:regular; text-align:center; margin-bottom:15px; ">포트폴리오에 묵혀둔 작품이 있나요? 제작비용 0원에 당신만의 제품을 선보이세요.<br>
 블랭킷은 신진 아티스트를 응원합니다.</p>
-    <div class="submit_button"><a href="#" class="myButton" style="color:">내 작품 판매하기</a></div>
+    <div class="submit_button"><a href="#" class="myButton" style="padding: 8px 35px; color:#687C87;">내 작품 판매하기</a></div>
 
   </div>
 </div>
@@ -371,7 +372,8 @@ $(window).resize(function(){
   <div class="row" style="margin-top:40px;">
     <div class="col-md-12">
     <p class="row_title">ARTIST</p>
-    <p class="row_desc">블랭킷의 디자이너들이다. 봐라</p>
+    <p class="row_desc">블랭킷에서 활동 중인 신진 아티스트들입니다.<br>
+그들의 특별한 작품들을 확인해 보세요.</p>
   </div>
   </div>
   <div class="row">
