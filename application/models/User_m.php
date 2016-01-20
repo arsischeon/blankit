ed92dd63ebd08ca40a255bcd81b9db590c46df84
ArsisCheon <arsischeon@gmail.com>
2016-01-20 12:55:02 +0900

    x

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User_m extends CI_Controller {
+class User_m extends CI_Model {
 	public function __construct(){
 		$this->load->database();
 	}
