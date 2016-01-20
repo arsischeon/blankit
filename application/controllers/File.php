41efa4e0e36fa36e82e1ca64c17b8505f01697a6
Woohyun Kim <r45r54r45@naver.com>
2016-01-20 11:56:30 +0900

    dd

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -5,25 +5,25 @@ class File extends CI_Controller {
 
 	public function profile()
 	{
-		// if (!empty($_FILES)) {
-		//   $tempFile = $_FILES['profile']['tmp_name'];
-		//   $targetPath = "/files/profile/";  //4
-		//   $targetFile =  $targetPath. $_FILES['profile']['name'];  //5
-		//   move_uploaded_file($tempFile,$targetFile); //6
+		if (!empty($_FILES)) {
+		  $tempFile = $_FILES['userfile']['tmp_name'];
+		  $targetPath = "/files/profile/";  //4
+		  $targetFile =  $targetPath. $_FILES['profile']['name'];  //5
+		  move_uploaded_file($tempFile,$targetFile); //6
+		}
+		// $config['upload_path'] = "/files/profile/";
+		//
+		// $this->load->library('upload', $config);
+		//
+		// if ( ! $this->upload->do_upload())
+		// {
+		// 	$error = array('error' => $this->upload->display_errors());
+		// 	echo "Sibal";
 		// }
-		$config['upload_path'] = "/files/profile/";
-
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-			echo "Sibal";
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-			echo "good";
-		}
+		// else
+		// {
+		// 	$data = array('upload_data' => $this->upload->data());
+		// 	echo "good";
+		// }
 	}
 }
