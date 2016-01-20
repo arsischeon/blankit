3b6402a9c41b6fe22bc686b2c2414a8db43e3267
ArsisCheon <arsischeon@gmail.com>
2016-01-20 15:40:46 +0900

    x

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -7,12 +7,13 @@ class Order_model extends CI_Model{
 	}
 	
 	public function cart(){
+		$user_id_SESSION = $this->session->userdata('user_id');
 		
 		return $this->db->query("
 		select user_name, user_email, user_phone, user_home, user_home2, user_home3, u.user_id as buyer, s.user_id as artist, s.store_id,
 			cart_type, cart_color, cart_size, cart_num, cart_price, cart_price_total, cart_status, store_name, store_random_id
 		from USER u, CART c, STORE s
-		where u.user_id = c.user_id and s.store_id = c.store_id and cart_status = 1 and c.user_id = $this->session->userdata('user_id');
+		where u.user_id = c.user_id and s.store_id = c.store_id and cart_status = 1 and c.user_id = '$user_id_SESSION';
 		");
 	}
 	
