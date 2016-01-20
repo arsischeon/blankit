4874a316dae0a6656f4b04ce03dc0b534d1869c3
ArsisCheon <arsischeon@gmail.com>
2016-01-20 12:54:03 +0900

    x

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -2,10 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_m extends CI_Controller {
+	public function __construct(){
+		$this->load->database();
+	}
 
 	var $details;
 	
-	function validate_user( $user_id, $user_pw ) {
+	public function validate_user( $user_id, $user_pw ) {
 		// Build a query to retrieve the user's details
 		// based on the received username and password
 		$this->db->from('USER');
@@ -26,7 +29,7 @@ class User_m extends CI_Controller {
 		return false;
 	}
 	
-	function set_session() {
+	public function set_session() {
 		// session->set_userdata is a CodeIgniter function that
 		// stores data in a cookie in the user's browser.  Some of the values are built in
 		// to CodeIgniter, others are added (like the IP address).  See CodeIgniter's documentation for details.
