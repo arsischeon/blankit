4cdef4ae76f57eea796b5eae9554b56b9082a779
ArsisCheon <arsischeon@gmail.com>
2016-01-21 14:34:23 +0900

    x

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -35,28 +35,37 @@ class Order extends CI_Controller {
 	}
 	function func_order_ok()
 	{
-		$receiver = $this->input->post('receiver');
-		$email = $this->input->post('email');
-		$phone1 = $this->input->post('phone1');
-		$phone2 = $this->input->post('phone2');
-		$phone3 = $this->input->post('phone3');
-		$home1 = $this->input->post('home1');
-		$home2 = $this->input->post('home2');
-		$home3 = $this->input->post('home3');
-		$payer = $this->input->post('payer');
-		$bank = $this->input->post('bank');
-		$account = $this->input->post('account');
-		$notice = $this->input->post('notice');
-		$totalPrice = $this->input->post('totalPrice');
-		$orderRandomId = $this->input->post('orderRandomId');
-
-		$this->load->model('order_model');
-		$this->order_model->order($receiver, $email, $phone1, $phone2, $phone3, $home1, $home2, $home3, $payer, $bank, $account, $notice, $totalPrice, $orderRandomId);
-
-		//완료 페이지로 redirect
-		$completeUrl = "http://blankit.kr/order/complete" . "?codeNum=" . $orderRandomId . "&bank=" . $bank . "&account=" . $account;
-		$this->load->helper('url');
-		redirect($completeUrl);
+		/*** POST 값이 없으면 home으로 redirect ***/
+		if ($orderRandomId = $this->input->post('orderRandomId')){
+			$receiver = $this->input->post('receiver');
+			$email = $this->input->post('email');
+			$phone1 = $this->input->post('phone1');
+			$phone2 = $this->input->post('phone2');
+			$phone3 = $this->input->post('phone3');
+			$home1 = $this->input->post('home1');
+			$home2 = $this->input->post('home2');
+			$home3 = $this->input->post('home3');
+			$payer = $this->input->post('payer');
+			$bank = $this->input->post('bank');
+			$account = $this->input->post('account');
+			$notice = $this->input->post('notice');
+			$totalPrice = $this->input->post('totalPrice');
+			//$orderRandomId = $this->input->post('orderRandomId');
+	
+			$this->load->model('order_model');
+			$this->order_model->order($receiver, $email, $phone1, $phone2, $phone3, $home1, $home2, $home3, $payer, $bank, $account, $notice, $totalPrice, $orderRandomId);
+	
+			//완료 페이지로 redirect
+			$completeUrl = "http://blankit.kr/order/complete" . "?codeNum=" . $orderRandomId . "&bank=" . $bank . "&account=" . $account;
+			$this->load->helper('url');
+			redirect($completeUrl);
+		}
+		else {
+			$homeUrl = "http://blankit.kr";
+			$this->load->helper('url');
+			redirect($homeUrl);
+		}
+		}
 
 	}
 }
