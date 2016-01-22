57b7b3ed49602bcc4cb9808cf250306f7a60a6cb
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 16:38:16 +0900

    dd

--- application/libraries/coolsms.php
+++ application/libraries/coolsms.php
@@ -33,14 +33,14 @@ class coolsms
 	}
 	public function curlProcess()
 	{
-		$ch = curl_init(); 
+		$ch = curl_init();
 		// 1 = POST , 0 = GET
 		if($this->method==1)
 			$host = sprintf("%s/%s/%s", $this->host, $this->version, $this->path);
 		elseif($this->method==0)
 			$host = sprintf("%s/%s/%s?%s", $this->host, $this->version, $this->path, $this->content);
 		curl_setopt($ch, CURLOPT_URL, $host);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($ch, CURLOPT_SSLVERSION,3); // SSL 버젼 (https 접속시에 필요)
 		curl_setopt($ch, CURLOPT_HEADER, 0); // 헤더 출력 여부
 		curl_setopt($ch, CURLOPT_POST, $this->method); // Post Get 접속 여부
@@ -48,11 +48,11 @@ class coolsms
 		if($this->method)
 		{
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $this->content); 
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $this->content);
 		}
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10); // TimeOut 값
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // 결과값을 받을것인지
-		
+
 		$this->result = json_decode(curl_exec($ch));
 		//Handle errors
 		if(curl_errno($ch))
@@ -105,10 +105,10 @@ class coolsms
 		$this->curlProcess();
 	}
 	/**
-	 * $method 
+	 * $method
 	 * GET = 0, POST, 1
 	 * $path
-	 * 'send' 'sent' 'cancel' 'balance' 
+	 * 'send' 'sent' 'cancel' 'balance'
 	 */
 	private function setMethod($path, $method)
 	{
@@ -125,25 +125,25 @@ class coolsms
 	 *	@type, image, refname, country, datetime, mid, gid, subject, charset (optional)
 	 *	@returns an object(recipient_number, group_id, message_id, result_code, result_message)
 	 */
-	public function send($options) 
+	public function send($options)
 	{
 		$this->setMethod('send', 1);
-		$this->addInfos($options);	
+		$this->addInfos($options);
 		//return $this->getResult();
 	}
-	
+
 	/**
 	 * 	@GET sent method
 	 * 	@param $options (options can be optional)
 	 * 	@count,  page, s_rcpt, s_start, s_end, mid, gid (optional)
 	 * 	@returns an object(total count, list_count, page, data['type', 'accepted_time', 'recipient_number', 'group_id', 'message_id', 'status', 'result_code', 'result_message', 'sent_time', 'text'])
 	 */
-	public function sent($options=null) 
+	public function sent($options=null)
 	{
 		if(!$options)
 			$options = new stdClass();
 		$this->setMethod('sent', 0);
-		$this->addInfos($options);	
+		$this->addInfos($options);
 		return $this->getResult();
 	}
 	/**
@@ -151,22 +151,22 @@ class coolsms
 	 * 	@options must contain api_key, salt, signature
 	 * 	@mid, gid (either one must be entered.)
 	 */
-	public function cancel($options) 
+	public function cancel($options)
 	{
 		$this->setMethod('cancel', 1);
-		$this->addInfos($options);	
+		$this->addInfos($options);
 		return $this->getResult();
 	}
-	
+
 	/**
 	 * 	@GET balance method
 	 * 	@options must contain api_key, salt, signature
 	 * 	@return an object(cash, point)
 	 */
-	public function balance() 
+	public function balance()
 	{
 		$this->setMethod('balance', 0);
-		$this->addInfos($options = new stdClass());	
+		$this->addInfos($options = new stdClass());
 		return $this->getResult();
 	}
 	/**
@@ -175,10 +175,10 @@ class coolsms
 	 * 	@return an object(registdate, sms_average, sms_sk_average, sms_kt_average, sms_lg_average, mms_average, mms_sk_average, mms_kt_average, mms_lg_average)
 	 * 	this method is made for Coolsms inc. internal use
 	 */
-	public function status($options) 
+	public function status($options)
 	{
 		$this->setMethod('status', 0);
-		$this->addInfos($options);	
+		$this->addInfos($options);
 		return $this->getResult();
 	}
 	private function objectToArray($d) {
@@ -187,8 +187,8 @@ class coolsms
 			// with get_object_vars function
 			$d = get_object_vars($d);
 		}
- 
-		if (is_array($d)) 
+
+		if (is_array($d))
 		{
 			/*
 			* Return array converted to object
@@ -197,13 +197,13 @@ class coolsms
 			*/
 			return array_map(__FUNCTION__, $d);
 		}
-		else 
+		else
 		{
 			// Return array
 			return $d;
 		}
 	}
-	function getOS() { 
+	function getOS() {
 		$user_agent = $this->user_agent;
 		$os_platform    =   "Unknown OS Platform";
 		$os_array       =   array(
@@ -231,11 +231,11 @@ class coolsms
 								'/blackberry/i'         =>  'BlackBerry',
 								'/webos/i'              =>  'Mobile'
 							);
-		foreach ($os_array as $regex => $value) { 
+		foreach ($os_array as $regex => $value) {
 			if (preg_match($regex, $user_agent)) {
 				$os_platform    =   $value;
 			}
-		}   
+		}
 		return $os_platform;
 	}
 	function getBrowser() {
@@ -252,7 +252,7 @@ class coolsms
 								'/konqueror/i'  =>  'Konqueror',
 								'/mobile/i'     =>  'Handheld Browser'
 							);
-		foreach ($browser_array as $regex => $value) { 
+		foreach ($browser_array as $regex => $value) {
 			if (preg_match($regex, $user_agent)) {
 				$browser    =   $value;
 			}
