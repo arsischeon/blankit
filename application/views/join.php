5e80d49374b061ff8d301100fb6e195f79b38025
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 15:30:50 +0900

    dd

--- application/views/join.php
+++ application/views/join.php
@@ -86,6 +86,12 @@ $(function(){
 
   //  $("#portfolio").dropzone({ url: "/file/post",maxFilesize:1 });
   // var myDropzone = new Dropzone("div#portfolio", { url: "/main",maxFilesize:1,maxFiles:1});
+
+  // $("#sibal").on("click",function(){
+  //   myDropzone.processQueue();
+  // });
+});
+$(window).on("load",function(){
   var myDropzone1 = new Dropzone("#profile", { url: "/file/profile", paramName: "userfile",maxFiles:1,addRemoveLinks:true,autoProcessQueue: false,dictRemoveFile:"파일 삭제",dictMaxFilesExceeded:"1개만 업로드할 수 있습니다."});
   myDropzone1.on("addedfile", function(file) {
   });
@@ -93,9 +99,6 @@ $(function(){
   var myDropzone2 = new Dropzone("#portfolio", { url: "/main", paramName: "portfolio",autoProcessQueue: false,maxFiles:0});
   myDropzone2.on("addedfile", function(file) {
   });
-  // $("#sibal").on("click",function(){
-  //   myDropzone.processQueue();
-  // });
 });
 Dropzone.autoDiscover = false;
 // Dropzone.options.portfolio = {
