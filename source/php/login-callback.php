5467dc5a81722bc478d2159f746305279228b1f9
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 15:03:19 +0900

    dd

--- source/php/login-callback.php
+++ source/php/login-callback.php
@@ -20,8 +20,7 @@ try {
 if (isset($accessToken)) {
   // Logged in!
   $_SESSION['facebook_access_token'] = (string) $accessToken;
-  header('Location: /');
-  exit;
+  echo "ff";
   // Now you can redirect to another page and use the
   // access token from $_SESSION['facebook_access_token']
 }
