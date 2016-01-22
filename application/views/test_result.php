f8c7d3609450a7f9fad166f6b962945e67126afe
ArsisCheon <arsischeon@gmail.com>
2016-01-22 14:26:26 +0900

    x

--- application/views/test_result.php
+++ application/views/test_result.php
@@ -6,7 +6,12 @@ $xxDeleteCart = explode(',', $deleteCart);
 
 $xxDeleteSize = sizeOf($xxDeleteCart);
 echo $xxDeleteSize;
-//$x=1;
+$x=1;
+
+while($x < $xxDeleteSize){
+	echo $xxDeleteCart[$x];
+	$x++;
+}
 
 /* for($x=1; x<$xxDeleteSize; $x++){
 	echo $xxDeleteCart[$x];
