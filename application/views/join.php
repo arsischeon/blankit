7523e8b196e5c9b0497cddede169ac19e69a8465
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 15:36:43 +0900

    nn

--- application/views/join.php
+++ application/views/join.php
@@ -273,7 +273,7 @@ function submitCheck(){
           </div>
           <div class="row row-padding-xs-100 " style="margin-top:10px;">
             <div class="menu title2" style="margin-bottom:10px;">프로필사진</div>
-            <form id="profile" class="dropzone" action="/file/profile" style="border-radius:10px;
+            <form id="profile" class="dropzone"  action="/file/profile" style="border-radius:10px;
             ">
             <div class="dz-default dz-message"><span>프로필 사진은 한 개만 업로드 할 수 있습니다<br>( 클릭 또는 파일을 드래그 해주세요! )</span></div>
           </form>
