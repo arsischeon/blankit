8641e514e69056c18bcebf50e5834dc621e38a8b
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 15:23:15 +0900

    ddd

--- application/views/join.php
+++ application/views/join.php
@@ -86,12 +86,12 @@ $(function(){
 
   //  $("#portfolio").dropzone({ url: "/file/post",maxFilesize:1 });
   // var myDropzone = new Dropzone("div#portfolio", { url: "/main",maxFilesize:1,maxFiles:1});
-  var myDropzone = new Dropzone("#profile", { url: "/file/profile", paramName: "userfile",maxFiles:1,addRemoveLinks:true,autoProcessQueue: false,dictRemoveFile:"파일 삭제",dictMaxFilesExceeded:"1개만 업로드할 수 있습니다."});
-  myDropzone.on("addedfile", function(file) {
+  var myDropzone1 = new Dropzone("#profile", { url: "/file/profile", paramName: "userfile",maxFiles:1,addRemoveLinks:true,autoProcessQueue: false,dictRemoveFile:"파일 삭제",dictMaxFilesExceeded:"1개만 업로드할 수 있습니다."});
+  myDropzone1.on("addedfile", function(file) {
   });
 
-  var myDropzone = new Dropzone("#portfolio", { url: "/main", paramName: "portfolio",autoProcessQueue: false,maxFiles:0});
-  myDropzone.on("addedfile", function(file) {
+  var myDropzone2 = new Dropzone("#portfolio", { url: "/main", paramName: "portfolio",autoProcessQueue: false,maxFiles:0});
+  myDropzone2.on("addedfile", function(file) {
   });
   // $("#sibal").on("click",function(){
   //   myDropzone.processQueue();
