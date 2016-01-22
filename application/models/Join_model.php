0a1b49c5a440ed5be34a1ff2d40eecb02f6f889e
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 16:12:44 +0900

    dd

--- application/models/Join_model.php
+++ application/models/Join_model.php
@@ -9,6 +9,9 @@ class Join_model extends CI_Model{
 
 	public function id_list(){ // 회원가입 join 페이지에서 id 중복확인 위해 모든 id 끌고오기
 		$id=$this->input->get("id");
+		echo "아이디는 ".$id;
+		echo "select count(*) from `USER` where user_id='".$id."';";
 		return $this->db->query("select count(*) from `USER` where user_id='".$id."';");
+
 	}
 }
