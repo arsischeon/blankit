c4376a0c7d49a560fd6275084f8e16b585f9144a
Woohyun Kim <r45r54r45@naver.com>
2016-01-22 16:17:25 +0900

    ddd

--- application/views/join.php
+++ application/views/join.php
@@ -70,6 +70,7 @@ function id_check(){
 $.ajax({
 url: "/join/id_check?id="+$("input[name='id']").val(),
 success:function(data){
+  console.log(data);
   if(data=='true'){
       $("#id_check").val("true");
       alert('사용할 수 있는 아이디입니다.');
