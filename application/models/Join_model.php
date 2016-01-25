deb75ae75c5c44d64474ea262088c118c4421b62
ArsisCheon <arsischeon@gmail.com>
2016-01-25 14:18:03 +0900

    x

--- application/models/Join_model.php
+++ application/models/Join_model.php
@@ -21,7 +21,7 @@ class Join_model extends CI_Model{
 	public function func_join_ok2($id, $pw, $name, $phone, $type, $job, $introduction){
 		$this->db->query("
 			insert into `USER` (user_id, user_pw, user_name, user_phone, user_type, artist_type, artist_intro)
-			values ($id, $pw, $name, $phone, $type, $job, $introduction)
+			values ('$id', '$pw', '$name', '$phone', '$type', '$job', '$introduction')
 			");
 	}
 }
