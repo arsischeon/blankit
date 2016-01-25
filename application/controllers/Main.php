02f816391b662462f5faa230e8a18da5a2d8d9e9
ArsisCheon <arsischeon@gmail.com>
2016-01-25 12:20:40 +0900

    x

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -7,11 +7,12 @@ class Main extends CI_Controller {
 	{		
 		$this->load->model('main_model');
 		$data['mdpick']=$this->main_model->mdpick();
+		$data2['dateChecker']=$this->main_model->dateChecker();
 
 		$this->load->model('artist_model');
 		$data['artist_list']=$this->artist_model->artist_list();
 		// $data['mdartist']=$this->main->mdartist();
-		$this->load->view('header');
+		$this->load->view('header', $data2);
 		$this->load->view('main',$data);
 		// 푸터는 메인페이지 안에 들어가있음
 	}
